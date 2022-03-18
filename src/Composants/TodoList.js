0138f11d5ac7c360d4eb14992d924a42704986cf
aboalsim114 <sami.abdulhalim@hetic.net>
2022-03-18 17:00:37 +0100

    1 file modified

--- src/Composants/TodoList.js
+++ src/Composants/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 export default function TodoList() {
   const [titre, setTitre] = useState(''); // input titre
